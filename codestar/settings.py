a59a74c9b5d2681ac669ef9978671aed9c24be53
Kate Elliott <kateelliott145@gmail.com>
2024-12-19 19:28:39 +0000

    Challenge: Deploy the project

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t44zc5ghybllra*(da&)7qhq=1pdzj%34-4i&dt-c-p3$@-yc1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-katee145-finalblogdjang-fy6legnkoq6.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-katee145-finalblogdjang-fy6legnkoq6.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
