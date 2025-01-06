86b866e5745229812f202557325fb0a1f2c1e705
Kate Elliott <kateelliott145@gmail.com>
2025-01-06 17:36:32 +0000

    update cloudinary api

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 #SECRET_KEY = 'django-insecure-t44zc5ghybllra*(da&)7qhq=1pdzj%34-4i&dt-c-p3$@-yc1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-katee145-finalblogdjang-fy6legnkoq6.ws.codeinstitute-ide.net','.herokuapp.com']
 
